c15fa27416e37db396ed524b520669bae5eed9a7
anonymous <anonymous@ANONYMOUSDEV>
2024-01-03 12:02:28 +0100

    Agregar archivos de proyecto.

---
+++ División entre dos números/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace División_entre_dos_números
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(15 / 3);
+            Console.Read();
+        }
+    }
+}
